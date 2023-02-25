8140bc2481ded1f2653cfe53bf6f1fd868efd529
eun6 <yy.eun6@gmail.com>
2023-02-25 20:09:20 +0900

    사용자 위치, 도시 api, 날씨 api 추가

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import { ActivityIndicator, View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
 import Geolocation from "@react-native-community/geolocation"; //커뮤니티 api
 import * as RNLocalize from "react-native-localize";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const { width : SCREEN_WIDTH } = Dimensions.get("window");
-
-console.log(RNLocalize.getLocales());
-console.log(RNLocalize.getCurrencies());
+const API_KEY = "5212ff3d3a90ee6b4c66779ca312c601";
+const icons = {
+    Rain : "rocket",
+    Clouds : "cloud",
+    Clear : "google"
+};
 
 //사용자에게 권한 허가 여부 받고
 //현재 사용자의 위치 정보 받아오기
@@ -14,9 +18,10 @@ Geolocation.setRNConfiguration("false", "whenInUse", "auto");
 
 export default function App() {
     const [city, setCity] = useState("Loading...");
+    const [days, setDays] = useState([]);
     const [latitude, setLatitude] = useState();
     const [longitude, setLongitude] = useState();
-    const getlocation =()=> {
+    const getlocation =async()=> {
         Geolocation.getCurrentPosition(
             postion => {
                 const latitude = postion.coords.latitude;
@@ -30,7 +35,15 @@ export default function App() {
             {enableHighAccuracy:true}
           );
           console.log(latitude, longitude);
-        setCity(RNLocalize.getTimeZone());
+        setCity(RNLocalize.getCountry);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+        const json = await response.json();
+        setDays(
+            json.list.filter((weather) => {
+            if (weather.dt_txt.includes("00:00:00")) {
+            return weather;}})
+        );
+        
     };
    useEffect(()=>{
     getlocation();
@@ -46,18 +59,33 @@ export default function App() {
             showsHorizontalScrollIndicator = {false}
             contentContainerStyle={styles.weather}>
         {/*일자별 예보의 view*/}
-            <View style={styles.day}>
-                <Text style={styles.temp}>27</Text>
-                <Text style={styles.description}>Sunny</Text>
-            </View>
-            <View style={styles.day}>
-                <Text style={styles.temp}>27</Text>
-                <Text style={styles.description}>Sunny</Text>
-            </View>
-            <View style={styles.day}>
-                <Text style={styles.temp}>27</Text>
-                <Text style={styles.description}>Sunny</Text>
-            </View>
+            {days.length === 0 ? (
+                <View style={{...styles.day, alignItems : "center"}}>
+                    <ActivityIndicator
+                        color ="white"
+                        style = {{marginTop: 10}}
+                        size = "large" 
+                    />
+                </View>
+            ):(
+                days.map((day, index)=> (
+                    <View key={index} style={styles.day}>
+                        <View
+                            style={{
+                                flexDirection : "row",
+                                alignItems : "center",
+                                justifyContent : "space-between"
+                            }}>
+                            <Text style={styles.temp}>
+                                {parseFloat(day.main.temp).toFixed(1)}
+                            </Text>
+                            <Icon name={icons[day.weather[0].main]} size={100} color="white"/>
+                        </View>
+                    <Text style={styles.description}>{day.weather[0].main}</Text>
+                    <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+                </View>
+                ))
+            )}      
         </ScrollView>
     </View>
 }
@@ -73,25 +101,32 @@ const styles = StyleSheet.create({
         alignItems:"center"
     },
     cityName : {
-        color:"black",
-        fontSize:70,
+        color:"white",
+        fontSize: 100,
         fontWeight:"500"
     },
     weather : {
-        
+        marginTop: -10,
     },
     day: {
         width: SCREEN_WIDTH, 
-        alignItems:"center",
+        paddingHorizontal: 10,
     },
     temp:{
-        color: "black",
+        color: "white",
         marginTop: 40,
-        fontSize: 180,
+        fontSize: 120,
     },
     description: {
-        marginTop:-45,
-        color: "black",
-        fontSize: 60,
+        marginTop:-20,
+        color: "white",
+        fontSize: 30,
+        paddingHorizontal: 10,
+    },
+    tinyText: {
+        fontSize: 20,
+        color: "white",
+        paddingHorizontal: 10,
+
     }
 });
